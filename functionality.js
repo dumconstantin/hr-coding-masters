2634839da5a37ec8978c109fbc44d3d24d53cf9a
Constantin Dumitrescu <dum.constantin@gmail.com>
2015-07-19 17:04:00 +0300

    Add timer.

--- functionality.js
+++ functionality.js
@@ -1,6 +1,7 @@
 function setColor(btn, color){
     var count=1;
     var property = document.getElementById(btn);
+    property.innerHTML = 'Done';
     if (count == 0){
         property.style.backgroundColor = "#FFFFFF"
         count=1;        
@@ -12,11 +13,15 @@ function setColor(btn, color){
 
 }
 
+var added = false;
+
 var comm = new Icecomm('MUlSAh1amhJSXKLdBXeUuvFzYMZLPAWM3as4c8JPJwQccMPtK');
 
 comm.connect('custom room', {audio: true});
 
 comm.on('connected', function(peer) {
+    if (added) return;
+    added = true;
     var remoteCandidate = peer.getVideo();
     remoteCandidate.setAttribute('id', 'remoteVideo');
    document.body.querySelector('#video').appendChild(remoteCandidate);
@@ -29,3 +34,17 @@ comm.on('local', function(peer) {
 comm.on('disconnect', function(peer) {
   document.getElementById(peer.ID).remove();
 }); 
+
+setTimeout(function () {
+    var time = document.querySelector('#time-lasted');
+    var started = moment();
+    var sec = 0;
+    setInterval(function () {
+        var now = moment();
+        var secs = now.diff(started, 'seconds');
+        var hour = Math.floor(secs / 3600);
+        var min = Math.floor((secs - (hour*3600)) / 60);
+        secs = Math.floor(secs % 60);
+        time.innerHTML = hour + ':' + min + ':' + secs;
+    }, 1000);
+});
